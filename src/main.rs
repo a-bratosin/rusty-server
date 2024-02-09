0b3b7de2d0a842816746810b84fc4ad5144a03f6
Andrei Bratosin <andreibratosin2004@gmail.com>
2024-02-09 23:58:32 +0200

    Added functionality for listening to TCP connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
+
 fn main() {
+    let listener = TcpListener::bind("127.0.0.1").unwrap();
+
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+
+        println!("Connection established! :D");
+    }
     println!("Hello, world!");
 }
