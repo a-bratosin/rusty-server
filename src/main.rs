ca5b833c167e5019fdb63eef2224e983e685a3b2
Andrei Bratosin <andreibratosin2004@gmail.com>
2024-02-10 22:49:40 +0200

    Added exception handling for busy/prohibited port

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::net::TcpListener;
+use std::process::exit;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:80");
+    match listener{
+        Err(_) => {
+            println!("Could not connect to the port!");
+            exit(1);
+        },
+        Ok(_)=>{println!("Connected to port!")},
+    }
 
-    for stream in listener.incoming(){
+
+    for stream in listener.unwrap().incoming(){
         let stream = stream.unwrap();
 
         println!("Connection established! :D");
