de4ab8f76833ac3d3d69d6f80862390bdcab3f4c
Andrei Bratosin <andreibratosin2004@gmail.com>
2024-02-12 23:49:36 +0200

    Added unconditional response to request from the client

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    net::{TcpListener, TcpStream},
-    io::{prelude::*, BufReader}
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
 };
 use std::process::exit;
 
@@ -33,4 +34,14 @@ fn handle_connection(mut stream: TcpStream){
         .collect();
 
     println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("../hello.html").unwrap();
+    let length = contents.len();
+
+    let response = 
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    
+    stream.write_all(response.as_bytes()).unwrap();
+
 }
