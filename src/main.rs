7625f59ad7adf6351cf9bb0079514b2650b52c54
Andrei Bratosin <andreibratosin2004@gmail.com>
2024-02-12 23:43:11 +0200

    The server now reads out the received request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader}
+};
 use std::process::exit;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:80");
+    let listener = TcpListener::bind("127.0.0.1:7878");
     match listener{
         Err(_) => {
             println!("Could not connect to the port!");
@@ -15,7 +18,19 @@ fn main() {
     for stream in listener.unwrap().incoming(){
         let stream = stream.unwrap();
 
-        println!("Connection established! :D");
+     //   println!("Connection established! :D");
+        handle_connection(stream);
     }
     println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
