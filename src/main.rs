f21f3d301adefe4a477df688a164e916431f9761
Andrei Bratosin <andreibratosin2004@gmail.com>
2024-02-16 23:45:03 +0200

    readded secret page

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn handle_connection(mut stream: TcpStream){
 
     let (status_line, filename) = match &request_line[..]{
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "media/hello.html"),
+        "GET /secret HTTP/1.1" => ("HTTP/1.1 200 OK", "media/secret.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "media/hello.html")
@@ -49,41 +50,5 @@ fn handle_connection(mut stream: TcpStream){
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
-
-    /*
-    if request_line == "GET / HTTP/1.1"{
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("media/hello.html").unwrap();    
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-        
-        stream.write_all(response.as_bytes()).unwrap();
-
-    }else if request_line == "GET /secret HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read("media/silly.jpeg").unwrap();
-        let length = contents.len();
-
-        let response = 
-            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n");
-        
-        stream.write_all(response.as_bytes()).unwrap();
-        stream.write_all(&contents).unwrap();
-    
-    }else{
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("media/404.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-        
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-    */    
+    stream.write_all(response.as_bytes()).unwrap();    
 }
