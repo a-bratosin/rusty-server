80df6105e11391f413779ce99dfbf2ffd9a81c49
Andrei Bratosin <andreibratosin2004@gmail.com>
2024-02-16 23:43:34 +0200

    switched from if statements to a match statement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{
     fs::{self, read_to_string},
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 use std::process::exit;
 
@@ -30,6 +32,26 @@ fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     
+
+
+    let (status_line, filename) = match &request_line[..]{
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "media/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "media/hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "media/404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+
+    /*
     if request_line == "GET / HTTP/1.1"{
         let status_line = "HTTP/1.1 200 OK";
         let contents = fs::read_to_string("media/hello.html").unwrap();    
@@ -63,5 +85,5 @@ fn handle_connection(mut stream: TcpStream){
         
         stream.write_all(response.as_bytes()).unwrap();
     }
-    
+    */    
 }
